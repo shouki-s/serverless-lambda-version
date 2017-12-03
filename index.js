aedd7bee176baf02ecca280cc63024273863fd49
Dan Van Brunt <dvanbrunt@gmail.com>
2017-12-03 01:10:10 -0500

    feat(stable): Stable build

--- index.js
+++ index.js
@@ -4,83 +4,69 @@ const _ = require("lodash");
 
 class LambdaArn {
   constructor(serverless, options) {
+    this.serverless = serverless;
+    this.options = options;
     this.hooks = {
-      "after:package:finalize": () => updateLambdaVersion(serverless)
+      "before:package:finalize": this.updateLambdaVersion.bind(this)
     };
   }
-}
 
-const getArnAndVersion = (resources, funcNormName) => {
-  const key = _.findKey(
-    resources,
-    r =>
-      r.Type === "AWS::Lambda::Version" &&
-      r.Properties.FunctionName.Ref === funcNormName
-  );
+  updateLambdaVersion() {
+    const resources = this.serverless.service.resources.Resources;
+    const compiledResources = this.serverless.service.provider
+      .compiledCloudFormationTemplate.Resources;
+    const lambdaArns = this.getResourcesWLambdaAssoc(resources);
 
-  return key
-    ? {
-        "Fn::Join": [
-          "",
-          [
-            { "Fn::GetAtt": [funcNormName, "Arn"] },
-            ":",
-            { "Fn::GetAtt": [key, "Version"] }
-          ]
-        ]
-      }
-    : undefined;
-};
+    _.forEach(lambdaArns, (value, key1) => {
+      const associations =
+        value.Properties.DistributionConfig.DefaultCacheBehavior
+          .LambdaFunctionAssociations;
 
-const getResourcesWAssoc = resources =>
-  _.pickBy(
-    resources,
-    r =>
-      r.Type === "AWS::CloudFront::Distribution" &&
-      r.Properties.DistributionConfig &&
-      r.Properties.DistributionConfig.DefaultCacheBehavior &&
-      r.Properties.DistributionConfig.DefaultCacheBehavior
-        .LambdaFunctionAssociations &&
-      r.Properties.DistributionConfig.DefaultCacheBehavior
-        .LambdaFunctionAssociations
-  );
-
-const updateLambdaVersion = serverless => {
-  const funcNormName = "RewriteLambdaFunction";
-  const resources =
-    serverless.service.provider.compiledCloudFormationTemplate.Resources;
-  const lambdaArns = getResourcesWAssoc(resources);
-
-  serverless.cli.log(
-    `lambdaArns: ${JSON.stringify(
-      serverless.service.provider.compiledCloudFormationTemplate.Resources
-    )}`
-  );
-
-  _.forEach(lambdaArns, (value, key1) => {
-    serverless.cli.log(`Key1: ${JSON.stringify(key1)}`);
-    serverless.service.provider.compiledCloudFormationTemplate.Resources[
-      key1
-    ] = {};
-
-    const associations =
-      resources[key1].Properties.DistributionConfig.DefaultCacheBehavior
-        .LambdaFunctionAssociations;
-
-    _.forEach(associations, (association, key2, associations) => {
-      const arn = association.LambdaFunctionARN;
-      const versionRef = getArnAndVersion(resources, arn);
-      if (arn && versionRef) {
-        serverless.cli.log(`Key2: ${JSON.stringify(key2)}`);
-        serverless.service.provider.compiledCloudFormationTemplate.Resources[
-          key1
-        ].Properties.DistributionConfig.DefaultCacheBehavior.LambdaFunctionAssociations[
-          key2
-        ].LambdaFunctionARN = versionRef;
-        serverless.cli.log(`getLambdaArns: ${JSON.stringify(versionRef)}`);
-      }
+      _.forEach(associations, (association, key2) => {
+        let arn = association.LambdaFunctionARN;
+        const versionRef = this.getArnAndVersion(compiledResources, arn);
+        if (arn && versionRef) {
+          association.LambdaFunctionARN = versionRef;
+        }
+      });
     });
-  });
-};
+  }
+
+  getArnAndVersion(resources, funcNormName) {
+    const key = _.findKey(
+      resources,
+      r =>
+        r.Type === "AWS::Lambda::Version" &&
+        r.Properties.FunctionName.Ref === funcNormName
+    );
+
+    return key
+      ? {
+          "Fn::Join": [
+            "",
+            [
+              { "Fn::GetAtt": [funcNormName, "Arn"] },
+              ":",
+              { "Fn::GetAtt": [key, "Version"] }
+            ]
+          ]
+        }
+      : undefined;
+  }
+
+  getResourcesWLambdaAssoc(resources) {
+    return _.pickBy(
+      resources,
+      r =>
+        r.Type === "AWS::CloudFront::Distribution" &&
+        r.Properties.DistributionConfig &&
+        r.Properties.DistributionConfig.DefaultCacheBehavior &&
+        r.Properties.DistributionConfig.DefaultCacheBehavior
+          .LambdaFunctionAssociations &&
+        r.Properties.DistributionConfig.DefaultCacheBehavior
+          .LambdaFunctionAssociations
+    );
+  }
+}
 
 module.exports = LambdaArn;
